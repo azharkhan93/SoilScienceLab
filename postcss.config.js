c598d155b1c6beb47f8241976f71c4d1b81436d6
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-01 00:52:59 +0530

    new changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    plugins: {}
-
-}
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        "./wwwroot/**/*.html",        // Static HTML files
+        "./Pages/**/*.cshtml",        // Razor Pages
+        "./Views/**/*.cshtml",        // MVC Views (if applicable)
+        "./Components/**/*.razor",    // Razor Components
+        "./Shared/**/*.razor",        // Shared Razor Components
+      ],
+  theme: { extend: {} },
+  plugins: [],
+};

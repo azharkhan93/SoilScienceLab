2f0ab2701d0584dc9b47e44db290d9a2dfaee6fc
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-01 23:19:33 +0530

    new data added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 export default {
-    content: [
-        "./Pages/**/*.razor",
-        "./Components/**/*.razor",
-        "./Shared/**/*.razor",
-        "./wwwroot/**/*.html",
-        "./**/*.razor",
-        "./**/*.cshtml"
-      ],       
-   
-  theme: { extend: {} },
+  content: [
+      "./Pages/**/*.razor",
+      "./Components/**/*.razor",
+      "./Shared/**/*.razor",
+      "./wwwroot/**/*.html",
+      "./**/*.razor",
+      "./**/*.cshtml",
+      "./Layout/**/*.razor",
+  ],
+  theme: {
+      extend: {},
+  },
   plugins: [],
 };

4a6e9ea478af1f92754a30f80b71ef2490e9bd54
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-02 01:31:58 +0530

    new chnages

--- obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
+++ obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0551c6d3200d2ecee8452703f30ff6b09239a6ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f54b1bca03718179106272708e0c3db4389b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

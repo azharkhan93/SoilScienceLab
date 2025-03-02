a779293f4aad09beea855f1331527d53976f49c2
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-02 18:18:36 +0530

    hero section added

--- obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
+++ obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19c8a5e55eb93230e4aea50b35160c1ecf18e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbcb3ef265f1923dac101b50ff3095b934ec3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

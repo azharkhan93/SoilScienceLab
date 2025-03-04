313d40db4717742df2956360b0d42678b91a66dd
azharkhan93 <azharkhan.work101@gmail.com>
2025-03-04 16:16:40 +0530

    new changes

--- obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
+++ obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea2f0f08c9130dfae940e46e77234af6679e8c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176c5877b4110f65f8189bb23edf67188aada981")]
 [assembly: System.Reflection.AssemblyProductAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

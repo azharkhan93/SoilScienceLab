06865ea4a2779a1d87f767f6f96b77078c7a3af2
azharkhan93 <azharkhan.work101@gmail.com>
2025-07-19 16:26:53 +0530

    Update AssemblyInformationalVersion and cache for CenterOfExcellence

--- obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
+++ obj/Debug/net9.0/CenterOfExcellence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de9f55ea3ec3a363b950374800dfc064473049f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a2c4367b020034d0d85f185b63cba0af6d8fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CenterOfExcellence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
